4f155c485444f025b624267e5d438b5e3582766e
syeda fatima noor <fatimanoorsyeda74@gmail.com>
2022-05-21 12:52:50 -0700

    work complete

---
+++ app.js
@@ -0,0 +1,451 @@
+//  var cityName=prompt("Enter your city name")
+//  if (cityName =="Karachi") {
+//      console.log("Welcome to the city of lights")
+//  } else {
+//     console.log("Welcome to "+" "+cityName)   
+//  }
+
+//  var gender=prompt("Enter your gender")
+//  if (gender =="male") {
+//     console.log("Good morning Sir")
+//  }
+//  else if(gender =="female"){
+//     console.log("Good morning maam") 
+//  } 
+//  else {
+//      console.log("enter valid gender")  
+//  }
+
+
+
+// var signalColor=prompt("Enter signal color")
+// if (signalColor=="red"){
+//   console.log("Must stop")
+// }
+// else if (signalColor=="yellow"){
+//   console.log("Ready to move")
+// }
+// else if (signalColor=="green"){
+//   console.log("Move now")
+// }
+// else{
+// console.log("enter proper color")
+// }
+
+//  var remainingFuel=prompt ("Enter remaining fuel")
+//  if (parseFloat(remainingFuel) <0.25) {
+//     console.log("Please refill the fuel in your car")
+
+//  } else {
+//      console.log("You don't need refill")
+//  }
+
+//q5
+
+
+
+//q6
+// var totalMarksObtained = prompt("Enter marks obtained")
+// var totalMarks = prompt("Enter total marks")
+// var percentage = (totalMarksObtained / totalMarks) * 100
+// if (percentage <= 100) {
+//     if (percentage >= 80) {
+//         document.write("Total Marks:" + totalMarks + "<br/>")
+//         document.write("Total Marks Obtained:" + totalMarksObtained + "<br/>")
+//         document.write("Percentage:" + percentage + "%" + "<br/>")
+//         document.write("Grade:" + "A1" + "<br/>")
+//         document.write("Remarks :" + "Excelent" + "<br/>")
+//     } else if (percentage >= 70) {
+//         document.write("Total Marks:" + totalMarks + "<br/>")
+//         document.write("Total Marks Obtained:" + totalMarksObtained + "<br/>")
+//         document.write("Percentage:" + percentage + "%" + "<br/>")
+//         document.write("Grade:" + "A" + "<br/>")
+//         document.write("Remarks :" + "good" + "<br/>")
+//     }
+//     else if (percentage >= 60) {
+//         document.write("Total Marks:" + totalMarks + "<br/>")
+//         document.write("Total Marks Obtained:" + totalMarksObtained + "<br/>")
+//         document.write("Percentage:" + percentage + "%" + "<br/>")
+//         document.write("Grade:" + "B" + "<br/>")
+//         document.write("Remarks :" + "needs improvement" + "<br/>")
+//     }
+//     else {
+//         document.write("Total Marks:" + totalMarks + "<br/>")
+//         document.write("Total Marks Obtained:" + totalMarksObtained + "<br/>")
+//         document.write("Percentage:" + percentage + "%" + "<br/>")
+//         document.write("Grade:" + "Fail" + "<br/>")
+//         document.write("Remarks :" + "Sorry" + "<br/>")
+
+//     }
+//     if (percentage >= 80) {
+//         document.write("Total Marks:" + totalMarks + "<br/>")
+//         document.write("Total Marks Obtained:" + totalMarksObtained + "<br/>")
+//         document.write("Percentage:" + percentage + "%" + "<br/>")
+//         document.write("Grade:" + "A1" + "<br/>")
+//         document.write("Remarks :" + "Excelent" + "<br/>")
+//     } else if (percentage >= 70) {
+//         document.write("Total Marks:" + totalMarks + "<br/>")
+//         document.write("Total Marks Obtained:" + totalMarksObtained + "<br/>")
+//         document.write("Percentage:" + percentage + "%" + "<br/>")
+//         document.write("Grade:" + "A" + "<br/>")
+//         document.write("Remarks :" + "good" + "<br/>")
+//     }
+//     else if (percentage >= 60) {
+//         document.write("Total Marks:" + totalMarks + "<br/>")
+//         document.write("Total Marks Obtained:" + totalMarksObtained + "<br/>")
+//         document.write("Percentage:" + percentage + "%" + "<br/>")
+//         document.write("Grade:" + "B" + "<br/>")
+//         document.write("Remarks :" + "needs improvement" + "<br/>")
+//     }
+//     else {
+//         document.write("Total Marks:" + totalMarks + "<br/>")
+//         document.write("Total Marks Obtained:" + totalMarksObtained + "<br/>")
+//         document.write("Percentage:" + percentage + "%" + "<br/>")
+//         document.write("Grade:" + "Fail" + "<br/>")
+//         document.write("Remarks :" + "Sorry" + "<br/>")
+//     }
+// }
+// else{
+//     alert("Enter correct marks")
+// }
+
+//Questio#7
+// var secretNumber=7 
+// var guessSecretno=prompt("Enter the secret number")
+// if (guessSecretno==secretNumber){
+//     console.log(' "Bingo!Correct answer." ')
+// }
+// else if(guessSecretno==secretNumber+1){
+//     console.log("Close enough to the correct answer.")
+// }
+// else{
+//     console.log("try again")
+// }
+
+//Question#8
+//var enterAnumber=prompt("Enter a number")
+//var remainder=enterAnumber%3
+//if (remainder==0) {
+//  console.log('This number is divisible by 3')
+//}
+//else{
+//  console.log('This number is not divisible by 3')
+//}
+
+//Question#9
+// var enterNumber=prompt("Enter a number")
+// if (enterNumber%2==0) {
+//     console.log("even number")
+// } else {
+//     console.log("odd number")
+// }
+
+//tem>=40 if
+//temp>=30 else if
+//temp>=20 else if
+// temp>=10 else
+
+//Question#10
+// var Temperature=prompt("Enter the temperature")
+// if (Temperature>=40) {
+//     console.log("Too hot")
+// }
+// else if (Temperature>=30) {
+//     console.log("Normal")
+// }
+// else if (Temperature>=20) {
+//     console.log("Cool")
+// }
+// else {
+//     console.log("OMG!sOO COOL")
+// }
+
+
+//CHAPTER 12-13
+
+//Question#1
+
+
+
+
+
+//Question#2
+// var A=prompt("Enter an integer")
+// var B=prompt("Enter another integer")
+// if (A>B) {
+//     console.log(A+ " "+"is larger")
+// }
+
+// else if (B==A) {
+//     console.log("Both the integers are equal.")
+// }
+// else{
+//      console.log(B+ " "+"is greater")
+
+// }
+
+
+//Question#3
+
+
+
+
+//Question#4
+//var character=prompt("Enter a character")
+//if (character=="a"|| character=="e"|| character=="i"|| character=="o" || character=="u") {
+//  console.log("True")
+//}
+//else{
+//  console.log(false)
+//}
+
+//Question#5
+// var password="admin"
+// var userPassword=prompt("Enter password")
+// if (userPassword==""){
+//     console.log("Please enter your password")
+// }
+// else if(userPassword==password) {
+//     console.log("'Correct! The password you have entered matches the original password' ")
+// }
+// else{
+//     console.log('"Incorrect Password" ')
+// }
+
+//Question#6
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// console.log("good day")
+// }
+// else{
+// greeting = "Good evening";
+// }
+
+//Question#7
+// var time=prompt=("Enter time")
+// if (time>=0000 && time<1200) {
+//     console.log("Good Morning")
+// }
+// else if(time>=1200 && time <1700) {
+//     console.log("Good Afternoon")
+// }
+// else if(time>=1700 && time<2100){
+//     console.log("Good Evening")
+// }
+// else if (time >=2100 && time<=2359){
+//     console.log('Good Night')
+// }
+// else{
+//     console.log('Enter valid time')
+// }
+
+
+//CHAPTER 14-16
+
+//Question#1
+//var studentName=[]
+
+//Question#2
+
+//Question#3
+//var stringArray=["apple","mango","grapes"]
+//console.log(stringArray[0])
+//Question#4
+//var numberArray=[1,2,4]
+
+//Question#4
+//var booleanArray=[true,false ]
+
+//Question#5
+//var mixedArray=["apple",1,2,true]
+
+//Question#6
+//var Arr=['SSC','HSC','BCS','BS','BCOM','MS','MPHILL','PHD']
+//for(var i=0;i<Arr.length;i++){
+//  var j=i+1
+//document.write(j+" "+Arr[i] +"<br/>")
+
+//}
+
+//Question#8
+// var Name=['Michael','John','Tony']
+// var Score=[320,230,480]
+// for (var i=0;i<=2;i++){
+//     var percent=(Score[i]/500)*100
+//     document.write("Score of"+" "+ Name[i]+" "+"is"+" "+Score[i]+" "+"Percentage:"+" "+percent+" "+"%"+" "+"<br/>")
+// }
+
+//Question#9
+//var colors=['red','yellow','green','blue','purple','pink','black'];
+
+//colors.unshift(prompt('Enter first color'))
+//console.log(colors)
+
+//colors.push(prompt("Enter last color"))
+//console.log(colors)
+
+//colors.unshift("grey", "white")
+//console.log(colors)
+
+//colors.shift()
+//console.log(colors)
+
+//colors.pop()
+//console.log(colors)
+
+//var j=prompt("enter index")
+// colors[j]=prompt("enter element")
+// console.log(colors)
+
+// var j=prompt("enter index")
+// var x=prompt("how many elements to delete?")
+// colors.splice(j,x)
+// console.log(colors)
+
+//Question#10
+// var  scoresOfstudents=[320,230,480,120];
+// console.log(scoresOfstudents)
+// console.log("ascending order"+scoresOfstudents.sort())
+
+
+//Question#11
+//var arr=['Karachi','Lahore','Islamabad','Quetta','Peshawar']
+//console.log(arr)
+//var selectedCities=arr.slice(2,4)
+//console.log(arr.slice(2,4));
+
+//q12
+//var arr=["This","is","my","cat"]
+//console.log("array:"+"\n"+arr.join())
+//console.log("string:"+"\n"+arr.join(" "))
+
+//Question#13
+// var array=[]
+// array.push('keyboard')
+// array.push('mouse')
+// array.push('printer')
+// array.push('monitor')
+// console.log(array)
+
+//array.shift()
+// console.log(array.shift())
+// console.log(array.shift())
+// console.log(array.shift())
+// console.log(array.shift())
+// console.log(array)
+
+//var array=[]
+//array.push('keyboard')
+//array.push('mouse')
+//array.push('printer')
+//array.push('monitor')
+//console.log(array)
+
+//console.log(array.pop())
+//console.log(array.pop())
+//console.log(array.pop())
+//console.log(array.pop())
+
+//console.log(array)
+
+//Arrays and Loops
+//Question#1
+//var arr=[[],[]];     multidimensional array
+
+//Question#2
+// var arr = [
+//     [0, 1, 2, 3],
+//     [1, 0, 1, 2],
+//     [2, 1, 0, 1]
+// ]
+// console.log(arr)
+
+//Question#3
+// for (var i=1; i<=10; i++){
+//     console.log(i)
+// }
+
+//Question#4
+//var tableNum=prompt("Enter table number")
+//var tableLength=prompt("Enter table length")
+//console.log('Multiplication table of'+" "+ tableNum );
+//console.log("Length"+" " + tableLength);
+//for (var i=1; i<=tableLength; i++){
+//  var result=tableNum*i
+//console.log(tableNum+'x'+i+ '='+ result)
+//}
+
+//Question#5
+// var fruits=['apple','banana','mango','orange','strawberry'];
+// for (var i=0; i<=4; i++){
+//     console.log("Element at index"+" "+i+" "+fruits[i])
+// }
+//Question#6
+// for (var i=1; i<=15; i++){
+//     console.log(i)
+// }
+//b
+// for (var i=10; i>=1; i--){
+//     console.log(i)
+// }
+//c
+// for (var i=0; i<=10; i++){
+//     console.log(i*2)
+// }
+
+//d
+// for (var i=1; i<=20; i+=2){
+//     console.log(i)
+// }
+// for (var i=1; i<=10; i++){
+//     console.log(i*2+"k")
+// }
+
+//Question#7
+// var A=["cake","apple pie","cookies","chips","patties"]
+// var searchByuser=prompt("Search your item");
+// for (var i=0; i<=A.length; i++){
+//     if (searchByuser==A[i]){
+//         alert(searchByuser+" "+ "is available at index" +" "+ i +" "+"in our bakery.")
+//     }
+//     else{
+//         console.log("We are sorry.Selected item is not available.")
+//     }
+// }
+
+//Question#8
+//var arr=[24,53,78,91,12]
+
+
+//var largest = arr[0];
+
+//for (var i = 0; i < arr.length; i++) {
+//if (largest < arr[i] ) {
+//  largest = arr[i];
+//    console.log(largest)
+//  }
+//}
+//console.log(largest);
+
+//Question#9
+//var arr = [24, 53, 78, 91, 12]
+//var smallest = arr[0];
+
+//for (var i = 0; i < arr.length; i++) {
+    //if (smallest > arr[i]) {
+      //  smallest = arr[i];
+    //    console.log(smallest)
+  //  }
+//}
+//console.log(smallest);
+
+
+//Question#10
+for (var i=1; i<=20; i++){
+  console.log(i*5)
+}
+
+
+
